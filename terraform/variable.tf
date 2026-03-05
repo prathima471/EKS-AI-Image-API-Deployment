67c327dd0a59a9ae74b05232098740b48efbef84
prathima471 <40650592+prathima471@users.noreply.github.com>
2026-03-05 16:28:08 -0500

    Change default environment variable to 'dev'

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -14,7 +14,7 @@ variable "project_name" {
 variable "environment" {
   description = "Environment name (dev, staging, prod)"
   type        = string
-  default     = "production"
+  default     = "dev"
 }
 
 # ── VPC ──
